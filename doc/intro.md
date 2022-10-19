c2162369893e855c27c052464250326959ce6c22
Magnus Dreyer <magnus.dreyer@ufst.dk>
2022-10-19 17:42:18 +0200

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fizz-buzz
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject fizz-buzz "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [org.clojure/data.json "0.2.6"]]
+  :main ^:skip-aot fizz-buzz.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
