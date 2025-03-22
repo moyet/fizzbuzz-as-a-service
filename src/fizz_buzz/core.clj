70caad54c053f588899f0e40e9c5429480c9bc81
Magnus Dreyer <magnus.dreyer@ufst.dk>
2025-03-22 15:15:44 +0100

    Exposes the API out of the container

--- src/fizz_buzz/core.clj
+++ src/fizz_buzz/core.clj
@@ -48,7 +48,8 @@
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     ; Run the server with Ring.defaults middleware
-    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port :host "0.0.0.0"})
+
     ; Run the server without ring defaults
     ;(server/run-server #'app-routes {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
