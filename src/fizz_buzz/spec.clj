8ae29fce100e7435921ab9b95c64ea6b38fe5cbc
Magnus Dreyer <magnus.dreyer@ufst.dk>
2025-04-04 21:16:51 +0200

    Specs added, give a failure when the input is wrong

--- src/fizz_buzz/spec.clj
+++ src/fizz_buzz/spec.clj
@@ -1,1 +1,7 @@
-(ns fizz-buzz.spec)
+(ns fizz-buzz.spec
+  (:require [clojure.spec.alpha :as s]))
+
+(s/def ::fizz-value integer?)
+(s/def ::fizz-name keyword? )
+(s/def ::fizz-buzz-rules (s/map-of ::fizz-name ::fizz-value))
+
